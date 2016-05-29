79a5617c8fa24c4b3c3593de3c44428b4944a992
hpulzl <824337531@qq.com>
2016-05-29 23:08:10 +0800

    获取评论列表，明天写添加评论模块。

---
+++ app/src/main/java/lzl/edu/com/movierecommend/entity/WeiReply.java
@@ -0,0 +1,40 @@
+package lzl.edu.com.movierecommend.entity;
+
+public class WeiReply {
+	private String wreplyID;
+	private String articleID;
+	private String userID;
+	private String wcontent;
+	private String wtime;
+	public String getWreplyID() {
+		return wreplyID;
+	}
+	public void setWreplyID(String wreplyID) {
+		this.wreplyID = wreplyID;
+	}
+	public String getArticleID() {
+		return articleID;
+	}
+	public void setArticleID(String articleID) {
+		this.articleID = articleID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getWcontent() {
+		return wcontent;
+	}
+	public void setWcontent(String wcontent) {
+		this.wcontent = wcontent;
+	}
+	public String getWtime() {
+		return wtime;
+	}
+	public void setWtime(String wtime) {
+		this.wtime = wtime;
+	}
+	
+}
---
+++ app/src/main/java/lzl/edu/com/movierecommend/entity/movieentity/WeiReplyCustom.java
@@ -0,0 +1,42 @@
+package lzl.edu.com.movierecommend.entity.movieentity;
+
+import java.io.Serializable;
+
+public class WeiReplyCustom implements Serializable{
+	private String wreplyID;
+	private String articleID;
+	private String userID;
+	private String wcontent;
+	private String wtime;
+	public String getWreplyID() {
+		return wreplyID;
+	}
+	public void setWreplyID(String wreplyID) {
+		this.wreplyID = wreplyID;
+	}
+	public String getArticleID() {
+		return articleID;
+	}
+	public void setArticleID(String articleID) {
+		this.articleID = articleID;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getWcontent() {
+		return wcontent;
+	}
+	public void setWcontent(String wcontent) {
+		this.wcontent = wcontent;
+	}
+	public String getWtime() {
+		return wtime;
+	}
+	public void setWtime(String wtime) {
+		this.wtime = wtime;
+	}
+	
+}
